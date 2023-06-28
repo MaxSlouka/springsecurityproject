77b9e80429fda40fa2f502dd85621a4efb5109de
MaxSlouka <jmslouka@gmail.com>
2023-06-28 09:53:57 +0200

    add methods for validation of tokens

--- springsecurityproject/src/main/java/com/example/springsecurityproject/config/JwtService.java
+++ springsecurityproject/src/main/java/com/example/springsecurityproject/config/JwtService.java
@@ -44,7 +44,21 @@ public class JwtService {
                 .compact();
         }
 
-        private Claims extractAllClaims(String token) {
+        public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+        }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
