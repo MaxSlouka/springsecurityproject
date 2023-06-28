d6e3ac694c7045eebab4db96e826fa12f234d627
MaxSlouka <jmslouka@gmail.com>
2023-06-28 13:39:36 +0200

    implement AuthRes. register method

--- springsecurityproject/src/main/java/com/example/springsecurityproject/auth/AuthenticationService.java
+++ springsecurityproject/src/main/java/com/example/springsecurityproject/auth/AuthenticationService.java
@@ -1,15 +1,34 @@
 package com.example.springsecurityproject.auth;
 
+import com.example.springsecurityproject.config.JwtService;
+import com.example.springsecurityproject.models.Role;
+import com.example.springsecurityproject.models.User;
+import com.example.springsecurityproject.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
     public AuthenticationResponse register(RegisterRequest request) {
-            return null;
+            var user = User
+                    .builder()
+                    .firstname(request.getFirstname())
+                    .lastname(request.getLastname())
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .role(Role.USER)
+                    .build();
+            userRepo.save(user);
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .build();
     }
 
     public AuthenticationResponse authentica(AuthenticationRequest request) {
